8b11ea637b72487dea79deed185fb5facdb6ff1f
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-03-17 16:38:33 +0530

    fixed w in getContractFactory

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -11,7 +11,7 @@ async function main() {
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
   so whitelistContract here is a factory for instances of our Whitelist contract.
   */
-  const whitelistContract = await ethers.getContractFactory("Whitelist");
+  const whitelistContract = await ethers.getContractFactory("whitelist");
 
 
   // here we deploy the contract
